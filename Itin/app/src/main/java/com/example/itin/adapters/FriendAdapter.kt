0bd7df986e1ddaa5821f997ec93f08cb7f66fedd
Colton Gann <44731467+Colton9810@users.noreply.github.com>
2022-03-18 16:26:38 -0500

    Add in requests
    Added in the ability to reconize who is a friend vs a friend request, display them accordingly, then be able to accept a request.

--- Itin/app/src/main/java/com/example/itin/adapters/FriendAdapter.kt
+++ Itin/app/src/main/java/com/example/itin/adapters/FriendAdapter.kt
@@ -3,16 +3,21 @@ package com.example.itin.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itin.R
 import com.example.itin.classes.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_friend.*
 import kotlinx.android.synthetic.main.friend_item.view.*
 
 class FriendAdapter (
-    private val friends: MutableList<User>
-): RecyclerView.Adapter<FriendAdapter.FriendViewHolder>(){
+    private val friends: MutableList<Pair<User, Boolean>>
+): RecyclerView.Adapter<FriendAdapter.FriendViewHolder>() {
 
-    inner class FriendViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class FriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.friend_item, parent, false)
@@ -20,10 +25,61 @@ class FriendAdapter (
     }
 
     override fun onBindViewHolder(holder: FriendAdapter.FriendViewHolder, position: Int) {
-        val curFriend = friends[position]
+        lateinit var curUser: DatabaseReference
+        lateinit var firebaseAuth: FirebaseAuth
+        lateinit var masterUserList: DatabaseReference
 
+
+        val curFriend = friends[position].first
+        val friend = friends[position].second
+
+        if (friend == true) {
+            holder.itemView.apply {
+                acceptButton.visibility = View.INVISIBLE
+                rejButton.visibility = View.INVISIBLE
+                friendFullName.text = curFriend.username
+            }
+        } else {
+            holder.itemView.apply {
+                acceptButton.visibility = View.VISIBLE
+                rejButton.visibility = View.VISIBLE
+                friendFullName.text = curFriend.username
+            }
+        }
         holder.itemView.apply {
-            friendFullName.text = curFriend.username
+            acceptButton.setOnClickListener {
+                masterUserList = FirebaseDatabase.getInstance().getReference("masterUserList")
+                firebaseAuth = FirebaseAuth.getInstance()
+                val firebaseUser = firebaseAuth.currentUser
+                val uid = firebaseUser!!.uid
+                val userName = curFriend.username
+                val friendsID = masterUserList.child(userName)
+                var myID = ""
+                var myUsername = ""
+
+                friendsID.get().addOnSuccessListener {
+                    if (it.exists()) {
+                        val friendsIDStr = it.value.toString()
+
+                        curUser.get().addOnSuccessListener {
+                            if (it.exists()) {
+                                myUsername = it.child("userInfo").child("username").value.toString()
+
+                                masterUserList.get().addOnSuccessListener {
+                                    if (it.exists()) {
+                                        myID = it.child(myUsername).value.toString()
+                                        val friendsUID =
+                                            it.child(friendsIDStr).child("UID").value.toString()
+
+                                        curUser.child("friendsList").child("Friend $friendsIDStr").setValue(friendsIDStr)
+                                        FirebaseDatabase.getInstance().getReference("users").child(friendsUID).child("friendsList")child("Friend $myID").setValue(myID)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
