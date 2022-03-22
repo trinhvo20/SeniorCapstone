12db9807a8085f896c238bfd1e76387aa0d1800c
Colton Gann <44731467+Colton9810@users.noreply.github.com>
2022-03-22 00:34:46 -0500

    UI Improvments
    Added many UI improvements to the friends list

--- Itin/app/src/main/java/com/example/itin/FriendActivity.kt
+++ Itin/app/src/main/java/com/example/itin/FriendActivity.kt
@@ -168,9 +168,10 @@ class FriendActivity : AppCompatActivity() {
                 curFriend.child(friendsUid).get().addOnSuccessListener {
                     if (it.exists()) {
                         val username = it.child("userInfo").child("username").value.toString()
+                        val uid = it.child("userInfo").child("uid").value.toString()
 
                         val user = User(
-                            "null",
+                            uid,
                             username,
                             "null",
                             "null"
