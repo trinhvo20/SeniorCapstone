e02a5c05fdd6f21b65a44bdb9a14b88f715c1078
Thomas <tschwartzenburg99@gmail.com>
2022-04-01 18:06:24 -0500

    Check .gitignore
    Make sure gitIgnore is working

--- Itin/app/src/main/java/com/example/itin/Constants.kt
+++ Itin/app/src/main/java/com/example/itin/Constants.kt
@@ -6,6 +6,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
+        // SERVER KEY IS A SECRET. SHOULD NOT BE FOUND IN CODE THAT ENDS UP IN GITHUB
         const val SERVER_KEY = BuildConfig.FCM_KEY
         const val CONTENT_TYPE = "application/json"
     }
