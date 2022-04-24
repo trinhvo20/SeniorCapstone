a4ab93b2218c584237202abee39d3347a00ac4a8
Thomas <tschwartzenburg99@gmail.com>
2022-04-23 22:36:23 -0500

    Made friends clickable

---
+++ Itin/app/src/main/java/com/example/itin/FriendInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.itin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_info)
+    }
+}
