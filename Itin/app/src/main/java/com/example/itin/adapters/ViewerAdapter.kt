964a3f006eafbe03ac694c6e32d6f924af6487bb
loggybayougator <gargresh99@gmail.com>
2022-04-28 09:57:40 -0500

    dropdown button hiding logic in place

--- Itin/app/src/main/java/com/example/itin/adapters/ViewerAdapter.kt
+++ Itin/app/src/main/java/com/example/itin/adapters/ViewerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itin.R
@@ -19,46 +20,7 @@ import java.io.File
 class ViewerAdapter(
     private val viewerList: MutableMap<String, Int>
 ) : RecyclerView.Adapter<ViewerAdapter.ViewerViewHolder>() {
-    inner class ViewerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        private val ivSetPerm: ImageView = itemView.findViewById(R.id.ivSetPerm)
-        private val clDropdown: ConstraintLayout = itemView.findViewById(R.id.clDropdown)
-        private val clOP1: ConstraintLayout = itemView.findViewById(R.id.clOP1)
-        private val clOP2: ConstraintLayout = itemView.findViewById(R.id.clOP2)
-        private val clOP3: ConstraintLayout = itemView.findViewById(R.id.clOP3)
-
-        private var open = false
-        init {
-            var curViewer = viewerList.keys.elementAt(adapterPosition)
-            var curViewerPerm = viewerList[curViewer]
-
-            clDropdown.removeAllViews()
-            ivSetPerm.setOnClickListener {
-                toggledropdown(curViewerPerm)
-            }
-        }
-
-        private fun toggledropdown(curViewerPerm: Int?) {
-            if(!open) {
-                if (curViewerPerm != 1 && curViewerPerm == 2 && curViewerPerm == 3) {
-                    clDropdown.addView(clOP1)
-                }
-
-                if (curViewerPerm == 1 && curViewerPerm != 2 && curViewerPerm == 3) {
-                    clDropdown.addView(clOP2)
-                }
-
-                if ((curViewerPerm == 1 && curViewerPerm == 2 && curViewerPerm != 3) || (curViewerPerm != 1 && curViewerPerm != 2 && curViewerPerm != 3)) {
-                    clDropdown.addView(clOP3)
-                }
-
-                open = true
-            }
-            else{
-                clDropdown.removeAllViews()
-                open = false
-            }
-        }
-    }
+    inner class ViewerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewerViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -68,9 +30,39 @@ class ViewerAdapter(
 
     override fun onBindViewHolder(holder: ViewerViewHolder, position: Int) {
         var curViewer = viewerList.keys.elementAt(position)
+        var open = false
         holder.itemView.apply {
 
-            tvPerm1.visibility = View.GONE
+            var curViewerPerm = viewerList[curViewer]
+            clOP1.visibility = View.GONE
+            clOP2.visibility = View.GONE
+            clOP3.visibility = View.GONE
+
+            ivSetPerm.setOnClickListener {
+                //Toast.makeText(context,"perm $curViewerPerm", Toast.LENGTH_LONG).show()
+                if(!open) {
+                    if (curViewerPerm != 1 && (curViewerPerm == 2 || curViewerPerm == 3)) {
+                        clOP1.visibility = View.VISIBLE
+                    }
+
+                    if (curViewerPerm != 2 && (curViewerPerm == 1 || curViewerPerm == 3)) {
+                        clOP2.visibility = View.VISIBLE
+                    }
+
+                    if ((curViewerPerm != 3 && (curViewerPerm == 1 || curViewerPerm == 2)) || (curViewerPerm != 1 && curViewerPerm != 2 && curViewerPerm != 3)) {
+                        clOP3.visibility = View.VISIBLE
+                    }
+
+                    open = true
+                }
+                else{
+                    clOP1.visibility = View.GONE
+                    clOP2.visibility = View.GONE
+                    clOP3.visibility = View.GONE
+                    open = false
+                }
+            }
+
 
 
             var storageReference = FirebaseStorage.getInstance().getReference("Users/$curViewer.jpg")
